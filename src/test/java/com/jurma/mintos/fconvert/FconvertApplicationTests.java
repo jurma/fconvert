79e342de4c87ee91e8f62e838781ffaae041c14f
Yuriy Martinen <jurmalare@gmail.com>
2024-01-18 11:03:21 +0200

    Spring boot context load test added

--- src/test/java/com/jurma/mintos/fconvert/FconvertApplicationTests.java
+++ src/test/java/com/jurma/mintos/fconvert/FconvertApplicationTests.java
@@ -1,19 +1,16 @@
 package com.jurma.mintos.fconvert;
 
-import com.jurma.mintos.fconvert.controller.AccountController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class FConvertApplicationTests {
-    @Autowired
-    private AccountController controller;
     @Test
-    void contextLoads() {
-        assertThat(controller).isNotNull();
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
     }
 
 }
